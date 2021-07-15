3a99d8700725586bf7c1b03c51ab364a1ad7d84b
NeuGuillen <86857089+NeuGuillen@users.noreply.github.com>
2021-07-15 13:42:54 -0300

    Hola

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,13 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla");
+	var mensaje;
+	var mensaje2;
+
+	mensaje="Hola mundo";
+	mensaje2="Esto funciona de maravilla";
+
+	alert(mensaje);
+	alert(mensaje2);
 }
 
